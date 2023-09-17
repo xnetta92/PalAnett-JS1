e7ba6e143f219b4b1510ffced66166c79dd9e281
xnetta92 <palanett92@gmail.com>
2023-09-17 21:54:27 +0200

    harmas

---
+++ feladat3.js
@@ -0,0 +1,9 @@
+/*const lista=[0,7,16,30,41]
+console.log(lista);
+lista.reverse();
+console.log(lista); ???*/  
+
+const lista=[0,7,16,30,41];
+
+for (let i = lista.length; i >= 0; i--) {console.log(lista[i]);}
+
